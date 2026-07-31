6956f5b583521e99bbeeaa7480fc80dabcb3be94
Andrew C <andr3wcalimlim@gmail.com>
2026-07-30 19:58:42 -0700

    checking exit codes and obtaining bash script output

--- github_PA_token_setup.py
+++ github_PA_token_setup.py
@@ -1,3 +1,5 @@
+import subprocess
+
 introduction_string = (
 	"\n" +
 	"In order to automate GitHub release deployment \n" +  
@@ -20,3 +22,53 @@ introduction_string = (
 	)
 
 print(introduction_string)
+
+supposed_token = \
+(
+	input(
+		"\n" + 
+		"Please enter a GitHub Personal Access Token meeting " + 
+		"the above requirements:" + 
+		"\n\n\t"
+	)
+
+)
+
+#subprocess.run("testVar='hello Shell'", shell=True)
+
+# Using subprocess class to run shell commands inside Python script
+# making std output of process when completed accessible
+# and turning the byte sequence into a string automatically
+
+# this is good info but there is a discrete easily acessible property
+# for checking if a process ran without errors
+
+# return code
+		# returns 0 when process has no issues
+		# returns 1 when process has stderror
+
+# documentation says exit code 0 typically means it ran successfully
+
+# we are still capturing output so that error isn't printed to std err console
+
+subprocess.run(	"gh auth logout",
+		shell=True,
+		capture_output=True)
+		
+testCompletedProcess = subprocess.run(	"gh auth login " + 
+					"--with-token " + 
+						f" <<< '{supposed_token}'", 
+					shell=True,
+					capture_output=True)
+
+# removing trailing white space
+
+#testString = testCompletedProcess.stderr.rstrip()
+
+print(testCompletedProcess.returncode)
+#print(testString)
+#print(len(testString))
+
+
+# TODO call bash and gh auth login PA token via txt file
+# for testing just pipe env var aka "github auth login <<< ($TOKEN)" 	
